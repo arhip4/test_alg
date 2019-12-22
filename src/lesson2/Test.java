bafcac43ce36f4b89264c841f70c30df5e2c2476
arhip4 <arhip4@gmail.com>
2019-12-23 00:15:27 +0500

    add selection sorting

--- src/lesson2/Test.java
+++ src/lesson2/Test.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Test<T extends Comparable> {
-    static Random random = new Random();
+  private  static Random random = new Random();
+
 
     public static void main(String[] args) {
+
         int[] a = new int[100000];
 
         for (int i = 0; i < 99999; i++) {
@@ -17,21 +19,52 @@ public class Test<T extends Comparable> {
 
         System.out.println(Arrays.toString(a));
 
-        int key;
-        for (int i = 1; i < a.length; i++) {
+        int x;
+        for (
+                int i = 1;
+                i < a.length; i++) {
             int j = i;
-            key = a[i];
-            while (j > 0 && key < a[j - 1]) {
+            x = a[i];
+            while (j > 0 && x < a[j - 1]) {
                 a[j] = a[j - 1];
                 j--;
             }
-            a[j] = key;
+            a[j] = x;
         }
 
         long timeOut = System.currentTimeMillis();
         System.out.println(Arrays.toString(a));
-        long timeResult = (timeOut - timeIn) ;
+        long timeResult = (timeOut - timeIn);
         System.out.println("Время сортировки: " + timeResult + " ms");
+
+        int[] b = new int[100000];
+
+        for (int i = 0; i < 99999; i++) {
+            b[i] = random.nextInt(100);
+        }
+
+        long timeIn1 = System.currentTimeMillis();
+
+        System.out.println(Arrays.toString(b));
+
+
+        for (int i = 0; i < b.length - 1; i++) {
+            int imin = i;
+            for (int j = i + 1; j < b.length; j++) {
+                if (b[j] < b[imin]) {
+                    imin = j;
+                }
+
+            }
+            int temp = b[i];
+            b[i] = b[imin];
+            b[imin] = temp;
+
+        }
+        long timeOut1 = System.currentTimeMillis();
+        System.out.println(Arrays.toString(b));
+        long timeResult1 = (timeOut1 - timeIn1);
+        System.out.println("Время сортировки: " + timeResult1 + " ms");
 
     }
 }
