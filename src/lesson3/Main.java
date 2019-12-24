fde5cd8cc728f7cd3b569e47142f0ec388e884fc
arhip4 <arhip4@gmail.com>
2019-12-24 20:37:51 +0500

    String to char

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -8,5 +8,9 @@ public class Main {
         String inputString = "Привет, мир!";
         char [] charArr = inputString.toCharArray();
         System.out.println(Arrays.toString(charArr));
+
+        for (int i = 0;i < inputString.length(); i++){
+            System.out.print(inputString.charAt(i) + " ");
+        }
     }
 }
