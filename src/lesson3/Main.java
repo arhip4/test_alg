3fbdc0dc3420f949466e83922d50dd963ac39189
arhip4 <arhip4@gmail.com>
2019-12-24 20:34:53 +0500

    String to char

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,7 +1,12 @@
 package lesson3;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<>(2);
+        //Stack<String> stack = new Stack<>(2);
+        String inputString = "Привет, мир!";
+        char [] charArr = inputString.toCharArray();
+        System.out.println(Arrays.toString(charArr));
     }
 }
--- src/lesson3/Stack.java
+++ src/lesson3/Stack.java
@@ -1,5 +1,7 @@
 package lesson3;
 
+import java.util.EmptyStackException;
+
 public class Stack<T> {
     private T[] list;
     private int size = 0;
@@ -16,6 +18,44 @@ public class Stack<T> {
         list = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
+    public void push(T value){
+        if(isFull()){
+            throw new StackOverflowError();
+        }
+        list[size]=value;
+        size++;
+    }
 
+    public T pop(){
+        T temp = peek();
+        size--;
+        list[size] = null;
+        return temp;
+    }
+
+    public T peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list[size-1];
+    }
+
+    public boolean isFull(){
+        return size == list.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void reCapacity(int newCapacity){
+        T[] tempArr = (T[]) new Object[newCapacity];
+        System.arraycopy(list,0,tempArr,0,size);
+        list = tempArr;
+    }
 
 }
